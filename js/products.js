6cccbb776d991a37a561ea0f240d6b32298321bb
LUISNEIM <luisneim@gmail.com>
2021-08-23 00:05:37 -0300

    first commit

--- js/products.js
+++ js/products.js
@@ -2,8 +2,5 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+  
 });
-fetch('https://japdevdep.github.io/ecommerce-api/product/all.json')
-  .then(response => response.json())
-  .then(data => console.log(data));
