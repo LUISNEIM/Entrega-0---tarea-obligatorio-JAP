5977c0e2aa3d2f1feec9d2918e361510bcb7dcfc
LUISNEIM <88439041+LUISNEIM@users.noreply.github.com>
2021-09-04 16:53:05 -0300

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,8 +1,9 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e);
-function red() { 
-   location.href = "index.html"; 
-}
-});
+/* 
+
+I built this login form to block the front end of most of my freelance wordpress projects during the development stage. 
+
+This is just the HTML / CSS of it but it uses wordpress's login system. 
+
+Nice and Simple
+
+*/
